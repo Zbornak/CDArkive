35de622252cb8fb61f6df86283bcd50883401b93
Mark <zbornack@protonmail.com>
2023-03-06 10:48:40 +0000

    Working on AddCD page

--- CDArkive/AddCD.swift
+++ CDArkive/AddCD.swift
@@ -12,11 +12,15 @@ struct AddCD: View {
     
     @State private var artist = ""
     @State private var format = ""
-    @State private var genre = ""
+    @State private var genre = "Rock"
     @State private var id = UUID()
+    @State private var language = "English"
     @State private var notes = ""
     @State private var rating = 3
     @State private var title = ""
+    
+    let genres = ["Rock", "Hip Hop", "Jazz", "Classical", "Opera", "American Songbook", "Metal", "Pop", "J-Pop", "Folk"]
+    let languages = ["English", "Afrikaans", "German", "Dutch", "Japanese", "Mandarin", "Polish"]
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
