c7b81e7036d0711489a70c1e12439b8d92badfba
Mark <zbornack@protonmail.com>
2023-03-06 09:58:17 +0000

    Initial Commit

---
+++ CDArkive/CDArkiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDArkiveApp.swift
+//  CDArkive
+//
+//  Created by Mark Strijdom on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CDArkiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
