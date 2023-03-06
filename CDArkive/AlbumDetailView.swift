d559e4ac538249bc05711c4ace46d1529aaaa35a
Mark <zbornack@protonmail.com>
2023-03-06 14:47:54 +0000

    Created album and single detail views

---
+++ CDArkive/AlbumDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  AlbumDetailView.swift
+//  CDArkive
+//
+//  Created by Mark Strijdom on 06/03/2023.
+//
+
+import SwiftUI
+
+struct AlbumDetailView: View {
+    let album: Album
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ CDArkive/SingleDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  SingleDetailView.swift
+//  CDArkive
+//
+//  Created by Mark Strijdom on 06/03/2023.
+//
+
+import SwiftUI
+
+struct SingleDetailView: View {
+    let single: Single
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
