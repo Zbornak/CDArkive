d3ecee838cf0d1267f875f3d69d2b0a0cc803db9
Mark <zbornack@protonmail.com>
2023-03-07 12:08:52 +0000

    Created function to change background color of detail view according to genre

--- CDArkive/AlbumDetailView.swift
+++ CDArkive/AlbumDetailView.swift
@@ -94,4 +94,34 @@ struct AlbumDetailView: View {
         try? moc.save()
         dismiss()
     }
+    
+    func backgroundColor() -> Color {
+        if album.genre == "Rock" {
+            return Color(red: 82 / 255, green: 61.2 / 255, blue: 49 / 255)
+        } else if album.genre == "Hip Hop" {
+            return Color(red: 65.9 / 255, green: 52.5 / 255, blue: 42.7 / 255)
+        } else if album.genre == "Jazz" {
+            return Color(red: 89.8 / 255, green: 63.1 / 255, blue: 45.5 / 255)
+        } else if album.genre == "Classical" {
+            return Color(red: 62 / 255, green: 71.4 / 255, blue: 61.2 / 255)
+        } else if album.genre == "Opera" {
+            return Color(red: 89 / 255, green: 73.7 / 255, blue: 57.6 / 255)
+        } else if album.genre == "American Songbook" {
+            return Color(red: 45.9 / 255, green: 30.2 / 255, blue: 43.5 / 255)
+        } else if album.genre == "Metal" {
+            return Color(red: 60.4 / 255, green: 55.7 / 255, blue: 75.3 / 255)
+        } else if album.genre == "Pop" {
+            return Color(red: 100 / 255, green: 46.7 / 255, blue: 96.1 / 255)
+        } else if album.genre == "J-Pop" {
+            return Color(red: 0 / 255, green: 64.7 / 255, blue: 76.1 / 255)
+        } else if album.genre == "Folk" {
+            return Color(red: 74.5 / 255, green: 89 / 255, blue: 91.4 / 255)
+        } else if album.genre == "Electro" {
+            return Color(red: 76.9 / 255, green: 42.4 / 255, blue: 76.1 / 255)
+        } else if album.genre == "Emo" {
+            return Color(red: 93.7 / 255, green: 86.7 / 255, blue: 62.4 / 255)
+        } else {
+            return Color(red: 64.7 / 255, green: 81.6 / 255, blue: 73.7 / 255)
+        }
+    }
 }
